fe3543f5ad113cace06a1bb8a56ec5f7898b9661
Waley Zhang (greedyAI) <wzzhangwaley@gmail.com>
2019-01-31 06:46:21 -0500

    Tundarra and mountains implemented

--- src/shaders/terrain-frag.glsl
+++ src/shaders/terrain-frag.glsl
@@ -7,7 +7,8 @@ in vec3 fs_Pos;
 in vec4 fs_Nor;
 in vec4 fs_Col;
 
-in float fs_Sine;
+in float fs_Biome;
+in float fs_Height;
 
 out vec4 out_Col; // This is the final output color that you will see on your
                   // screen for the pixel that is currently being processed.
@@ -15,5 +16,18 @@ out vec4 out_Col; // This is the final output color that you will see on your
 void main()
 {
     float t = clamp(smoothstep(40.0, 50.0, length(fs_Pos)), 0.0, 1.0); // Distance fog
-    out_Col = vec4(mix(vec3(0.5 * (fs_Sine + 1.0)), vec3(164.0 / 255.0, 233.0 / 255.0, 1.0), t), 1.0);
+    int biome = int(fs_Biome);
+    if (biome == 0) {
+      float snow = smoothstep(0.0, 0.5, clamp(fs_Height, 0.0, 0.5));
+      out_Col = vec4(mix(mix(vec3(0.25, 0.25, 0.25), vec3(1.0, 1.0, 1.0), snow), vec3(164.0 / 255.0, 233.0 / 255.0, 1.0), t), 1.0);
+    } else if (biome == 1) {
+      if (fs_Height < 0.01) {
+        out_Col = vec4(mix(vec3(0.0, 0.0, 1.0), vec3(164.0 / 255.0, 233.0 / 255.0, 1.0), t), 1.0);
+      } else if (fs_Height < 2.0) {
+        out_Col = vec4(mix(vec3(0.0, 0.5, 0.0), vec3(164.0 / 255.0, 233.0 / 255.0, 1.0), t), 1.0);
+      } else {
+        float snow = smoothstep(0.5, 1.0, clamp(fs_Height, 0.5, 1.0));
+        out_Col = vec4(mix(mix(vec3(0.0, 0.5, 0.0), vec3(1.0, 1.0, 1.0), snow), vec3(164.0 / 255.0, 233.0 / 255.0, 1.0), t), 1.0);
+      }
+    }
 }
--- src/shaders/terrain-vert.glsl
+++ src/shaders/terrain-vert.glsl
@@ -1,5 +1,5 @@
 #version 300 es
-
+precision highp float;
 
 uniform mat4 u_Model;
 uniform mat4 u_ModelInvTr;
@@ -14,25 +14,169 @@ out vec3 fs_Pos;
 out vec4 fs_Nor;
 out vec4 fs_Col;
 
-out float fs_Sine;
+out float fs_Biome;
+out float fs_Height;
 
-float random1( vec2 p , vec2 seed) {
+float random1(vec2 p, vec2 seed) {
   return fract(sin(dot(p + seed, vec2(127.1, 311.7))) * 43758.5453);
 }
 
-float random1( vec3 p , vec3 seed) {
+float random1(vec3 p, vec3 seed) {
   return fract(sin(dot(p + seed, vec3(987.654, 123.456, 531.975))) * 85734.3545);
 }
 
-vec2 random2( vec2 p , vec2 seed) {
+vec2 random2(vec2 p, vec2 seed) {
   return fract(sin(vec2(dot(p + seed, vec2(311.7, 127.1)), dot(p + seed, vec2(269.5, 183.3)))) * 85734.3545);
 }
+
+vec2 randvec1(vec2 n, vec2 seed) {
+    float x = sin(dot(n + seed, vec2(131.32, 964.31)));
+    float y = sin(dot(n + seed, vec2(139.345, 132.89)));
+    vec2 v = fract(329.779f * vec2(x, y));
+    return vec2(2.0 * v.x - 1.0, 2.0 * v.y - 1.0);
+}
+
+vec2 randvec2(vec2 n, vec2 seed) {
+    float x = sin(dot(n + seed, vec2(113.2, 634.11)));
+    float y = sin(dot(n + seed, vec2(109.5, 242.8)));
+    return fract(3242.177f * vec2(x, y));
+}
+
+float quinticSmooth(float t) {
+  return t * t * t * (t * (t * 6.0  - 15.0) + 10.0);
+}
+
+float perlinNoise(float x, float z) {
+  float gridWidth = 10.0;
+  vec2 seed = vec2(0.0, 0.0);
+
+  vec2 pos = vec2(x, z);
+  vec2 corner1 = vec2(gridWidth * floor(x / gridWidth), gridWidth * floor(z / gridWidth));
+  vec2 corner2 = vec2(corner1.x + gridWidth, corner1.y);
+  vec2 corner3 = vec2(corner1.x + gridWidth, corner1.y + gridWidth);
+  vec2 corner4 = vec2(corner1.x, corner1.y + gridWidth);
+
+  if (length(pos - corner1) == 0.0) {
+    corner1 += vec2(-0.01, -0.01);
+  } else if (length(pos - corner2) == 0.0) {
+    corner2 += vec2(0.01, -0.01);
+  } else if (length(pos - corner3) == 0.0) {
+    corner3 += vec2(0.01, 0.01);
+  } else if (length(pos - corner4) == 0.0) {
+    corner4 += vec2(-0.01, 0.01);
+  }
+
+  vec2 normalize1 = vec2(1.0, 0.0);
+  vec2 normalize2 = vec2(1.0, 0.0);
+  vec2 normalize3 = vec2(1.0, 0.0);
+  vec2 normalize4 = vec2(1.0, 0.0);
+  if (length(random2(corner1, seed)) != 0.0) {
+    normalize1 = normalize(randvec1(corner1, seed));
+  }
+  if (length(random2(corner2, seed)) != 0.0) {
+    normalize2 = normalize(randvec1(corner2, seed));
+  }
+  if (length(random2(corner3, seed)) != 0.0) {
+    normalize3 = normalize(randvec1(corner3, seed));
+  }
+  if (length(random2(corner4, seed)) != 0.0) {
+    normalize4 = normalize(randvec1(corner4, seed));
+  }
+
+  float dot1 = dot(normalize1, normalize(pos - corner1));
+  float dot2 = dot(normalize2, normalize(pos - corner2));
+  float dot3 = dot(normalize3, normalize(pos - corner3));
+  float dot4 = dot(normalize4, normalize(pos - corner4));
+
+  float fractX = vec2(pos - corner1).x / gridWidth;
+  float fractY = vec2(pos - corner1).y / gridWidth;
+
+  float i1 = mix(dot1, dot2, quinticSmooth(fractX));
+  float i2 = mix(dot4, dot3, quinticSmooth(fractX));
+
+  return mix(i1, i2, quinticSmooth(fractY));
+}
+
+float interpRand(float x, float z) {
+  vec2 seed = vec2(0.0, 0.0);
+
+    float intX = floor(x);
+    float fractX = fract(x);
+    float intZ = floor(z);
+    float fractZ = fract(z);
+
+    vec2 c1 = vec2(intX, intZ);
+    vec2 c2 = vec2(intX + 1.0, intZ);
+    vec2 c3 = vec2(intX, intZ + 1.0);
+    vec2 c4 = vec2(intX + 1.0, intZ + 1.0);
+
+    float v1 = random1(c1, seed);
+    float v2 = random1(c2, seed);
+    float v3 = random1(c3, seed);
+    float v4 = random1(c4, seed);
+
+    float i1 = mix(v1, v2, quinticSmooth(fractX));
+    float i2 = mix(v3, v4, quinticSmooth(fractX));
+    return mix(i1, i2, quinticSmooth(fractZ));
+}
+
+float fbmHeight(float x, float z, int biome) {
+  float total = 0.0;
+  float persistence = 0.25f;
+  int octaves = 8;
+
+  for (int i = 0; i < octaves; i++) {
+    float freq = pow(2.0, float(i));
+    float amp = pow(persistence, float(i));
+    if (biome == 0) {
+      total += perlinNoise(x * freq, z * freq) * amp;
+    } else if (biome == 1) {
+      total += interpRand(x * freq, z * freq) * amp;
+    }
+  }
+  return total;
+}
+
+vec2 tempAndRainfall(vec2 pos) {
+    float factor = 256.0;
+    vec2 seed = vec2(0.0, 0.0);
+
+    int x = int(floor(pos.x / factor));
+    int y = int(floor(pos.y / factor));
+    vec2 minWorley = factor * randvec2(pos, seed) + vec2(float(x) * factor, float(y) * factor);
+    float minDist = distance(minWorley, pos);
+    for (int i = x - 1; i <= x + 1; i++) {
+        for (int j = y - 1; j <= y + 1; j++) {
+            vec2 worley = factor * randvec2(vec2(float(i), float(j)), seed) + vec2(float(i) * factor, float(j) * factor);
+            if (minDist > distance(pos, worley)) {
+                minDist = distance(pos, worley);
+                minWorley = worley;
+            }
+        }
+    }
+    return random2(minWorley, seed);
+}
+
+
 
 void main()
 {
   fs_Pos = vs_Pos.xyz;
-  fs_Sine = (sin((vs_Pos.x + u_PlanePos.x) * 3.14159 * 0.1) + cos((vs_Pos.z + u_PlanePos.y) * 3.14159 * 0.1));
-  vec4 modelposition = vec4(vs_Pos.x, fs_Sine * 2.0, vs_Pos.z, 1.0);
+
+  float x = vs_Pos.x + u_PlanePos.x;
+  float z = vs_Pos.z + u_PlanePos.y;
+  vec2 biomeAttr = tempAndRainfall(vec2(x, z));
+
+  float tempBoundary = 0.5;
+  float rainBoundary = 0.5;
+  if (biomeAttr.y < rainBoundary) {
+    fs_Biome = 0.0;
+    fs_Height = fbmHeight(x, z, int(fs_Biome)) * 2.0;
+  } else {
+    fs_Biome = 1.0;
+    fs_Height = fbmHeight(x, z, int(fs_Biome)) * 4.0;
+  }
+  vec4 modelposition = vec4(vs_Pos.x, fs_Height, vs_Pos.z, 1.0);
   modelposition = u_Model * modelposition;
   gl_Position = u_ViewProj * modelposition;
 }
